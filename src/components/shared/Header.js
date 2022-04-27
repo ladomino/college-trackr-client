fd06dff38d0e7eeb8773f23b22710799fe8611ff
ldomino <domino.laurie@gmail.com>
2022-04-27 14:35:37 -0500

    Modify navbar color - setup home page

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -43,7 +43,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-	<Navbar bg='primary' variant='dark' expand='md'>
+	<Navbar className='navbar-custom' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
                 College Trackr
