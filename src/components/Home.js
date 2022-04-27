891c75b269a737b94b793d3e674f8c9b49fde98a
ldomino <domino.laurie@gmail.com>
2022-04-27 14:12:28 -0500

    Modifying home page image.  Link to login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom'
+
+const linkStyle = {
+	color: 'brown',
+	textDecoration: 'none'
+}
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
-			<h2>Home Page</h2>
+			<h2 className="home-text-header"><Link to='/login/' style={linkStyle}>Start Your Journey</Link></h2>
+			<img className="home-image" width="100%" src="https://i.imgur.com/Woa11EW.jpg"></img>
 		</>
 	)
 }
